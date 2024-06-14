bbcb7cf4b9ed25940ab7b8bcc00062f678f9a7f3
Canius-p <boiinventer@gmail.com>
2024-06-14 18:19:00 +0545

    query parameter

--- Day14/src/index.js
+++ Day14/src/index.js
@@ -15,9 +15,29 @@ const Mocuser = [
   { id: 4, username: 'Aj', name: 'Alice Johnson' },
   { id: 5, username: 'Bj', name: 'Bob Johnson' },
   { id: 6, username: 'Ab', name: 'Alice Smith' },
+  { id: 7, username: 'Bb', name: 'Bob Brown' },
+  { id: 8, username: 'Aa', name: 'Alice Adams' },
+  { id: 9, username: 'Bbrn', name: 'Bob Brown' },
 ];
 app.get('/api/users', (req, res) => {
   res.status(200).send(Mocuser);
+  console.log(req.query);
+  const {
+    query: { filter, value },
+  } = req;
+  if (!filter && !value) {
+    res.status(200).send(Mocuser);
+    return;
+  }
+  if (filter && value) {
+    const findUser = Mocuser.filter(user => user[filter].includes(value));
+    if (!findUser) {
+      res.status(404).send({ message: 'User not found' });
+      return;
+    }
+    res.status(200).send(findUser);
+    return;
+  }
 });
 
 app.get('/api/users/:id', (req, res) => {
