3c0fb86df9b5751bf0a34133af187c2d02d24ed0
Canius-p <boiinventer@gmail.com>
2024-06-16 18:23:30 +0545

    router extra

--- Day16/src/router/routerUser.js
+++ Day16/src/router/routerUser.js
@@ -9,4 +9,24 @@ router.get('/api/users', (req, res) => {
   });
 });
 
+router.post('/api/users', (req, res) => {
+  return res.status(201).json({
+    message: 'Data created successfully',
+    data: req.body,
+  });
+});
+
+router.put('/api/users/:id', (req, res) => {
+  return res.status(200).json({
+    message: 'Data updated successfully',
+    data: req.body,
+  });
+});
+router.delete('/api/users/:id', (req, res) => {
+  return res.status(200).json({
+    message: 'Data deleted successfully',
+    data: req.body,
+  });
+});
+
 export default router;
